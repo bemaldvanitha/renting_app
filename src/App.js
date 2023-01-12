ef102d91be7fdd3fab488f4e17639a53c2e5a4f3
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-01-12 10:46:02 +0530

    firebase config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import AddPropertyScreen from "./screeens/AddPropertyScreen";
+
 import {  } from './firebase/index';
 
 function App() {
   return (
     <div>
-      <AddPropertyScreen/>
+
     </div>
   );
 }

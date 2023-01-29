341ce259800fbb138f080ae777714547f260b2b3
sampathdissanayaka <dmcsampath97@gmail.com>
2023-01-29 19:48:08 +0530

    complete detailsScreen page

--- src/screeens/DetailsScreen.js
+++ src/screeens/DetailsScreen.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import {  } from 'antd';
+import { Card, Carousel, Image, Col, Avatar } from 'antd';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../firebase/index';
 
+const { Meta } = Card;
 const DetailsScreen = () => {
   const [property, setProperty] = useState({});
   const [error, setError] = useState('');
@@ -20,6 +21,8 @@ const DetailsScreen = () => {
           const house = { ...docSnap.data() };
           house.id = docSnap.id;
           setProperty(house);
+          console.log(property);
+          
       } else {
           setError('No Property Found');
       }
@@ -27,6 +30,37 @@ const DetailsScreen = () => {
 
   return (
       <div>
+
+<Card
+    style={{
+      width: 1000,
+      
+    }}
+    cover={ 
+                <Carousel autoplay={true}>
+                    { property.imageUrls.map(img => {
+                        return(
+                            <div key={img}>
+                                <Image
+                                    width={400}
+                                    src={img}
+                                />
+                            </div>
+                        )
+                    }) }
+                </Carousel>    
+    }
+   
+  >
+    <Meta
+      title= {property.title}
+      description={property.description}
+    />
+    <p>Bathrooms : {property.bathrooms}</p>
+    <p>Bedrooms : {property.bedrooms}</p>
+    <p>Location : {property.location}</p>
+    <p>price : {property.price} LKR</p>
+  </Card>
 
       </div>
     );

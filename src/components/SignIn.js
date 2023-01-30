c7ea026e8855e5f5d90e67f8c7bf093a32f081c9
randiayeshanikossinna <randiayeshani@gmail.com>
2023-01-30 10:32:32 -0800

    Signin page styling is added

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import { Input, Button, Alert } from 'antd';
+import { Input, Button, Alert,Layout ,Form} from 'antd';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import '../styles/SignIn.css'
+const {  Content } = Layout;
+
 
 function SignIn() {
   const [email, setEmail] = useState('');
@@ -25,17 +28,30 @@ function SignIn() {
   };
 
   return (
-    <div>
-      <div>
-          <Input id={'email'} placeholder={'enter your email'} value={ email } onChange={(e) => setEmail(e.target.value)}/>
+    <Layout className="layout">
+    <div className="container" >
+     
+      <Content style={{
+          padding: '0 400px',
+          margin: '100px',
+        }}>
+           <Form style={{maskBorder:'solid', borderRadius:'10px', borderBlockColor:'white', backgroundColor:'#f6fff8', padding:'15px'}}>
+      <div className='input-feild-email' >
+          <Input id={'email'} placeholder={'enter your email'} value={ email } onChange={(e) => setEmail(e.target.value)} />
       </div>
-      <div>
+      <div className='input-feild-pw'>
           <Input.Password id={'password'} placeholder={'enter your password'} value={ password }
-                          onChange={(e) => setPassword(e.target.value)}/>
+                          onChange={(e) => setPassword(e.target.value)} />
       </div>
-      <Button onClick={handleSubmit}>Sign In</Button>
+      <div className='signIn'>
+      <Button onClick={handleSubmit} style={{backgroundColor:'#ae2012',color:'white',width:'270px'}}>Sign In</Button></div>
         { isError && <Alert message={ alertMsg } type="error" /> }
+        </Form>
+        </Content>
+       
     </div>
+    
+    </Layout>
   );
 }
 

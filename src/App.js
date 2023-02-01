c0b88d6b336462f33ca4d34f9066ab74d8e9e136
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-02-01 18:05:32 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import DetailsScreen from "./screeens/DetailsScreen";
 import {  } from './firebase/index';
 
 function App() {
   return (
     <div>
-      <DetailsScreen/>
+
     </div>
   );
 }

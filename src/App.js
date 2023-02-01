e83074311b67113939833325292831b53846b645
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-02-01 18:04:23 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import AddPropertyScreen from "./screeens/AddPropertyScreen";
+import React from 'react';
 import { } from './firebase/index';
 import CusHeader from "./components/Header";
-import HomeList from "./screeens/Home";
-
-import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
-      <CusHeader />
-        <Routes>
-            <Route path='/' element={<HomeList />} />
-            <Route path='/add-property' element={<AddPropertyScreen />} />
-
-        </Routes>
-
+        <CusHeader />
     </div>
   );
 }

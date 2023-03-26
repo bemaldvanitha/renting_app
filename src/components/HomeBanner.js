17a876eb65da88ca2cace1bf4c8971d670b2ce41
chamaracz <126459870+chamaracz@users.noreply.github.com>
2023-03-26 23:50:16 +0530

    update search

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -20,7 +20,7 @@ const HomeBanner = () => {
     return (
 
         <section className='h-full max-h-[640px] mb-8 xl:mb-24'>
-             {/* <SearchBar changeSearchTxt={ changeSearch }/> */}
+             <SearchBar changeSearchTxt={ changeSearch }/>
             <div className='flex flex-col lg:flex-row'>
                 <div className='lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0'>
                     <h1 className='text-4xl lg:text-[58px] font-semibold leading-none mb-6'>
@@ -37,7 +37,7 @@ const HomeBanner = () => {
                 </div>
             </div>
 
-            {/* <Search changeFilters={ changeFilters } /> */}
+            <Search changeFilters={ changeFilters } />
             <AppCard filters={filters} search={ search }/>
         </section>
     )
